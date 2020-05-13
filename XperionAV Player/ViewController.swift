a59ebfd8d336ecad6369053bf77b7b4e9e316e49
Arjun <arjun.ps@experionglobal.com>
2020-05-13 12:38:15 +0530

    Initial Commit

---
+++ XperionAV Player/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XperionAV Player
+//
+//  Created by Arjun on 13/05/20.
+//  Copyright © 2020 Arjun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
